9f20b35a209827bc692e4610e334ceaf9adbc9ac
muhekaSA <3332190068@untirta.ac.id>
2021-08-22 23:48:01 +0700

    Solved for Integration Two cameras at the same time

--- dataset_generate.py
+++ dataset_generate.py
@@ -11,24 +11,45 @@ def generate_dataset():
             cropped_face = img[y:y+h,x:x+w]
         return cropped_face
     
-    vid_realTime = cv2.VideoCapture(0)
-    img_id = 0
+    vid_realTime0 = cv2.VideoCapture(0)
+    vid_realTime1 = cv2.VideoCapture(1)
+    img_ida = 0
+    img_idb = 0
     
     while True:
-        ret, frame = vid_realTime.read()
-        if face_cropped(frame) is not None:
-            img_id+=1
-            face = cv2.resize(face_cropped(frame), (200,200))
+        
+        ret0, frame0 = vid_realTime0.read()
+        ret1, frame1 = vid_realTime1.read()
+        
+        if face_cropped(frame0) is not None:
+            img_ida+=1
+            face = cv2.resize(face_cropped(frame0), (200,200))
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-            source_output_img = "Face_Dataset/"+"eka."+str(img_id)+'.jpg'
+            source_output_img = "Face_Dataset/"+"eka."+str(img_ida)+'.jpg'
             cv2.imwrite(source_output_img, face)
-            cv2.putText(face, str(img_id), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2 )
+            cv2.putText(face, str(img_ida), (50,50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2 )
             
             cv2.imshow("Cropped_Face", face)
-            if cv2.waitKey(1)==13 or int(img_id)==1000:
+            if cv2.waitKey(1)==13 or int(img_ida)==100:
                 break
+        
                 
-    vid_realTime.release()
+            
+        if face_cropped(frame1) is not None:
+            img_idb+=1
+            face2 = cv2.resize(face_cropped(frame1), (200,200))
+        
+            source_output_img2 = "Face_Dataset/RGB/"+"ekaRGB."+str(img_idb)+'.jpg'
+            cv2.imwrite(source_output_img2, face2)
+            cv2.putText(face2, str(img_idb), (50,50), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,0), 2 )
+            
+            cv2.imshow("Cropped_Face2", face2)
+            if cv2.waitKey(1)==13 or int(img_idb)==100:
+                break
+       
+    vid_realTime0.release()
+    vid_realTime1.release()
+    
     cv2.destroyAllWindows()
-    print("Collecting is Done... !!!")
+    print("Collecting is Done... !!!")              
 generate_dataset()
